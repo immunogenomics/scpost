4c6e2a4cf6aeb8d76398517d645cce1a74693899
nmillard10 <nmillard@g.harvard.edu>
2020-11-20 14:32:22 -0500

    Added ra_FibObj

--- R/data.R
+++ R/data.R
@@ -20,3 +20,14 @@
 #' @source \url{https://www.immport.org/shared/study/SDY998}
 "ra_PreHarmObj"
 
+#' List of metadata and PC embeddings for only the fibroblasts from the RA dataset before running Harmony integration. 
+#' Thie was used for Fig 4a
+#'
+#' @format 
+#'   meta: data.frame of 5,265 rows (cells) and 10 columns. "clusAllCells" refers to
+#'   cluster assignments from clustering all of the RA data, while "clusOnlyFib"
+#'   refers to cluster assignments from clustering only the fibroblasts (this was used in Fig 4a)
+#'   embeddings: data.frame of 5,265 row (cells) and 20 columns (PCs).
+#' 
+#' @source \url{https://www.immport.org/shared/study/SDY998}
+"ra_FibObj"
